a940f637eca554ad891801280948c04be77e741c
teang1995 <teang1995@ajou.ac.kr>
2024-07-06 21:23:25 +0900

    #6 solved

--- baekjoon/10986.py
+++ baekjoon/10986.py
@@ -7,16 +7,14 @@ nums = [int(x) for x in sys.stdin.readline().split()]
 ans = 0
 residuals = [0 for _ in range(M)]
 
-dp = [0 for _ in range(len(nums))]
-
 sum_ = 0
-cnt = 0
+
 for i, num in enumerate(nums):
     sum_ += num
-    dp[i] = sum_
     residuals[sum_ % M] += 1
 
 for res in residuals:
     ans += (res * (res - 1)) //2
+    
 ans += residuals[0]
 print(ans)
