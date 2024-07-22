2a2d7c7f1702c67e6f968ef0d17c883f76f94e34
yongtaek.lim <teang1995@naver.com>
2024-07-22 03:01:55 +0000

    #13 solved

---
+++ baekjoon/17298.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/17298
+
+n = int(input())
+A = list(map(int, input().split()))
+answer = [-1] * n
+stack = []
+
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*answer)
